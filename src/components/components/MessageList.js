2a195932acc99f558e4481c35e4594f5b819e4dc
tsbajwa <teranjit@gmail.com>
2017-08-26 07:11:05 +1000

    Added timestamp to messages

--- src/components/components/MessageList.js
+++ src/components/components/MessageList.js
@@ -4,7 +4,7 @@ export default function MessageList(props) {
   return (
     <ul>
       {props.thread.messages.map((m) => (
-        <li key={m.id} onClick={() => props.onClick(m.id)}>{m.text}</li>
+        <li key={m.id} onClick={() => props.onClick(m.id)}>{m.text} @{m.time}</li>
       ))}
     </ul>
   )
