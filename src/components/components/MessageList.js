f57877c8efc4d47119c1bb95ec93d1869a432d9f
tsbajwa <teranjit@gmail.com>
2017-08-26 08:02:20 +1000

    Added SASS support

--- src/components/components/MessageList.js
+++ src/components/components/MessageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MessageList(props) {
   return (
-    <ul>
+    <ul className='messagelist-list'>
       {props.thread.messages.map((m) => (
         <li key={m.id} onClick={() => props.onClick(m.id)}>{m.text} @{m.time}</li>
       ))}
